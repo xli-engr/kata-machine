837166eac58e47e96dba25b7cbc52d5ffc21a9a7
xli-engr <seanli.job@gmail.com>
2023-03-31 12:02:24 -0700

    update

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,21 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
 
+    let start = 0;
+    let end = haystack.length - 1;
+
+    while (start + 1 < end) {
+        const mid = Math.floor(start + (end - start) / 2);
+        if (haystack[mid] >= needle) {
+            end = mid;
+        } else {
+            start = mid;
+        }
+    }
+
+    if (haystack[start] === needle || haystack[end] === needle) {
+        return true;
+    }
+
+    return false;
+
 }
