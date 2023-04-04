70e5d00733b1d7fd8a5e2e6f74e5e4ce7a1f7a3a
xli-engr <seanli.job@gmail.com>
2023-04-04 05:13:25 -0700

    update

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,23 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
 
+    const jmpAmount = Math.floor(Math.sqrt(breaks.length));
+    let breakPoint = 0;
+
+    for (let i = 0; i < breaks.length; i += jmpAmount) {
+        if (breaks[i]) {
+            breakPoint = i;
+            break;
+        }
+    }
+
+    let startPoint = breakPoint - jmpAmount;
+
+    for (let i = startPoint; i <= breakPoint; i++) {
+        if (breaks[i]) {
+            return i;
+        }
+    }
+
+    return -1;
+
 }
